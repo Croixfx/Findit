ae622024e63b06d7959ad94bdc91937c1afc2a41
Croixfx <jeanniyonkuru29@gmail.com>
2026-05-06 11:39:12 +0200

    Fix Firebase config for Render deployment

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const admin = require('firebase-admin');
 
-const serviceAccountPath = path.resolve(__dirname, '..', process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
-const serviceAccount = require(serviceAccountPath);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  const serviceAccountPath = path.resolve(__dirname, '..', process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
+  serviceAccount = require(serviceAccountPath);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
